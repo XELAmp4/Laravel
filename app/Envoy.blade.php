954c755895d1270636849bcda23e16876bab67a6
XELAmp4 <alexandre.bellet0321@gmail.com>
2024-02-09 08:58:29 +0100

    prod

--- app/Envoy.blade.php
+++ app/Envoy.blade.php
@@ -1,6 +1,9 @@
 @servers(['web' => ['alexandre-bellet@13.39.150.46'], 'workers' => ['alexandre-bellet@13.39.150.46']])
  
 @task('put-into-prod', ['on' => 'workers'])
+    cd alexandre-bellet.dhonnabhain.me
+    git checkout production
+    cd app
     composer update
     npm install
     npm run build
@@ -8,4 +11,5 @@
     composer install --optimize-autoloader --no-dev
     php artisan optimize:clear
     php artisan optimize
+    php artisan key:generate
 @endtask
